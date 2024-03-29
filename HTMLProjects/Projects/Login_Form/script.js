5854c04a710c10d7292fc99ddcf195c3628b0c3e
Samriddhi <samriddhis@743@gmail.com>
2024-03-29 21:17:05 +0530

    00

--- HTMLProjects/Projects/Login_Form/script.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('form').addEventListener('submit', function(event) {
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-  
-    if (username === '' || password === '') {
-      alert('Please fill in all fields');
-      event.preventDefault();
-    }
-  });  
